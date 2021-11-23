8e123c6bed2ec7c71621323d8a3d99e28a60a702
NoLifeKing <webmaster@itssimple.se>
2021-11-23 23:40:06 +0100

    refactor: Removed unused usings

--- CurseForge.Minecraft.Serverpack.Launcher/Program.cs
+++ CurseForge.Minecraft.Serverpack.Launcher/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.CommandLine;
-using System.CommandLine.Builder;
 using System.CommandLine.Invocation;
-using System.CommandLine.Parsing;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
